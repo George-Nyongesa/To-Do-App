888dbd5c0fb6d822045940daa342e04fbb9ba20a
Frello <frello@Frello.Frello>
2024-05-01 07:23:52 -0400

    Added an event listener to the Enter key of the keyboard in the todo.js file

--- todo.js
+++ todo.js
@@ -16,6 +16,24 @@ function addTask(){
     }
     inputBox.value='';
 }
+//Adding an event listener to the Enter key of the keyboard
+inputBox.addEventListener('keydown', (e) => {
+    if(e.key ==='Enter'){
+        if(inputBox.value===''){
+            alert("You must add text!");
+        }
+        else{
+            let li=document.createElement('li');
+            li.innerHTML=inputBox.value;
+            listContainer.appendChild(li);
+            let span=document.createElement('span');
+            span.innerHTML='\u00d7';
+            li.appendChild(span);
+            saveData();
+        }
+        inputBox.value='';
+    }
+});
 listContainer.addEventListener('click', function(e){
     if(e.target.tagName==='LI'){
         e.target.classList.toggle('checked');
